5a712d48a811e6944a920effd7a76f2e05febd82
Piyush <piyushdravyakar48@gmail.com>
2024-04-03 19:39:32 +0530

    commit 2

--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -5,17 +5,27 @@ from Utilities.loggerfile import LogGen
 
 class TestLogin:
     log = LogGen.logger()
+    username = "piyushdravyakar48@gmail.com"
+    password = "Piyush@123"
 
     def test_logins(self,setup):
         self.log.info("**********Exicution Started*********")
         self.driver = setup
         self.lp=LoginPage(self.driver)
-        self.lp.set_username("piyushdravyakar48@gmail.com")
-        self.lp.set_password("Piyush@123")
+        self.lp.set_username(self.username)
+        self.log.info("Your Username is: %s -", self.username)
+        self.lp.set_password(self.password)
+        self.log.info("Your password is: %s -", self.password)
         self.lp.login()
+        self.log.info("Click on Login button")
         self.emailcheck = self.driver.find_element(By.XPATH,"//p/strong").text
         if self.emailcheck == "piyushdravyakar4":
             print("test case Passed")
+            self.log.info("************Email verified************")
+
         else:
             print("Test case failed")
+            self.log.error("Test case is failed")
             self.driver.save_screenshot(r"C:\Users\ASUS\PycharmProjects\pythonProject1\Screenshots\login.png")
+
+        self.log.info("**********Exicution Succesful*********")
